791eefd59180bcb1b978821af8eb7cd268103b8a
sanji0219 <81353953+sanji0219@users.noreply.github.com>
2022-07-11 00:42:43 +0900

    stuReco

--- 220708 homepage/corporate/src/Top.tsx
+++ 220708 homepage/corporate/src/Top.tsx
@@ -5,39 +5,39 @@ function Top() {
     return (
         // Ucc6497084246731ce06ff3668f70dbce
         <div className="Top">
-            <header>
-                <h1><span>with</span>Uto</h1>
-                <div className="hamburger">
-                    <i className="fa-solid fa-bars fa-3x off" />
-                </div>
-                <div className="hamburger">
-                    <i className="fa-solid fa-minus fa-3x on" />
-                </div>
-                <ol>
-                    <li className='top'><p>トップ</p></li>
-                    <li className='intro'><p>自己紹介</p></li>
-                    <li className='works'><p>作品</p></li>
-                    <li className='about'><p>withUtoについて</p></li>
-                    <li className='message'><p>問い合わせ</p></li>
-                </ol>
-            </header>
-            <main>
-                <div className="content top">
-                    <img src="./images/withuto.png" alt="withutologo" />
-                </div>
-                <div className='content intro'>
-                    <img src="./images/withuto.png" alt="withutologo" />
-                </div>
-                <div className='content works'>
-                    <img src="./images/withuto.png" alt="withutologo" />
-                </div>
-                <div className='content about'>
-                    <img src="./images/withuto.png" alt="withutologo" />
-                </div>
-                <div className='content message'>
-                    <img src="./images/withuto.png" alt="withutologo" />
-                </div>
-            </main>
+        <header>
+            <h1><span>with</span>Uto</h1>
+            <div className="hamburger">
+                <i className="fa-solid fa-bars fa-3x off" />
+            </div>
+            <div className="hamburger">
+                <i className="fa-solid fa-minus fa-3x on" />
+            </div>
+            <ol>
+                <li className='top'><p>トップ</p></li>
+                <li className='intro'><p>自己紹介</p></li>
+                <li className='works'><p>作品</p></li>
+                <li className='about'><p>withUtoについて</p></li>
+                <li className='message'><p>問い合わせ</p></li>
+            </ol>
+        </header>
+        <main>
+            <div className="content top">
+                <img src="./images/withuto.png" alt="withutologo" />
+            </div>
+            <div className='content intro'>
+                <img src="./images/withuto.png" alt="withutologo" />
+            </div>
+            <div className='content works'>
+                <img src="./images/withuto.png" alt="withutologo" />
+            </div>
+            <div className='content about'>
+                <img src="./images/withuto.png" alt="withutologo" />
+            </div>
+            <div className='content message'>
+                <img src="./images/withuto.png" alt="withutologo" />
+            </div>
+        </main>
         </div>
     );
 }
---
+++ 220709 flsk/heroku/with/static/js/script.js
@@ -0,0 +1,39 @@
+$(()=>{
+    var nowScroll=$(this).scrollTop();
+    var contents=[".catchcopy",".goal",".inYearGoal"]
+    contents.forEach((i)=>{
+        $(i).hide()
+    })
+    $(window).scroll(()=> {
+        var scroll=$(this).scrollTop();
+        // console.log(scroll)
+        if(scroll<1000){
+            if (scroll>nowScroll){
+                $(".lowerRight").css("top",`-=${(scroll-nowScroll)/3}`)
+                $(".topTitle").css({"top":`+=${(nowScroll-scroll)*3/11}`,"font-size":`+=${(nowScroll-scroll)/10}`})
+            }else{
+                $(".lowerRight").css("top",`+=${(nowScroll-scroll)/3}`)
+                $(".topTitle").css({"top":`+=${(nowScroll-scroll)*3/11}`,"font-size":`+=${(nowScroll-scroll)/10}`})
+            }
+            nowScroll=scroll;
+        }
+        if(scroll>=1000){
+            $(".lowerRight").css("top","8.6px")
+            $(".topTitle").css({"top":"0.9px","font-size":"44.8px"})
+        }
+        if(scroll==0){
+            $(".lowerRight").css("top","40vh")
+            $(".topTitle").css({"top":"32vh","font-size":"17vh"})
+        }
+        var top=1000;
+        contents.forEach((content)=>{
+            if(scroll>=top+50 && scroll<=top+450){
+                $(content).fadeIn()
+            }
+            if(scroll<top || scroll>top+500){
+                $(content).fadeOut(100)
+            }
+            top+=500
+        })
+    });
+})
